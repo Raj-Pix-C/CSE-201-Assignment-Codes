a64a0804a4d5366e6b78f05eef690ebf8debdfc3
Raj <raj.2302017@std.hstu.ac.bd>
2025-02-12 23:58:53 +0600

    update ch6

--- fc.py
+++
@@ -1,8 +0,0 @@
-for i in range(1, 13):
-    file_name = f"ch6_q{i}.cpp"
-    with open(file_name, "w") as file:
-        file.write('#include <iostream>\nusing namespace std;\n\n')
-        file.write('int main() {\n\t')
-        file.write('\n\treturn 0;\n}')
-
-print("success ^u^")
