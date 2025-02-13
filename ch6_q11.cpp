fb456d0f5a4357a2c329ebdc84fe2710c67a96af
Raj <raj.2302017@std.hstu.ac.bd>
2025-02-13 20:42:12 +0600

    update 0.2

--- ch6_q11.cpp
+++ ch6_q11.cpp
@@ -1,7 +1,77 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
+class Fraction {
+	public:
+		int num;
+		int denum;
+
+		void input() {
+			char slash;
+			cin >> num >> slash >> denum;
+		}
 	
-	return 0;
+		void display() const {
+			cout << num << "/" << denum;
+		}	
+	};
+
+class FracCalc {
+private:
+    Fraction a, b, r;
+
+public:
+    void input() {
+		cout << "Enter first number: ";
+		a.input();
+		cout << "Enter second number: ";
+		b.input();
+        menu();
+    }
+
+    void menu() {
+        char op;
+        cout << "Enter operator (+, -, *, /): ";
+        cin >> op;
+        switch(op) {
+            case '+':
+                r.num = a.num*b.denum+b.num*a.denum;
+				r.denum = a.denum*b.denum;
+                break;
+            case '-':
+				r.num = a.num*b.denum-b.num*a.denum;
+				r.denum = a.denum*b.denum;
+                break;
+            case '*':
+				r.num = a.num*b.num;
+				r.denum = a.denum*b.denum;
+                break;
+            case '/':
+				r.num = a.num*b.denum;
+				r.denum = a.denum*b.num;
+                break;
+            default:
+                cout << "Invalid operator!";
+                break;
+        }
+		int gcd = __gcd(r.num, r.denum);
+		r.num /= gcd;
+		r.denum /= gcd;
+		r.display();
+
+        char ch;
+        cout << endl << "Do another (y/n)? ";
+        cin >> ch;
+        if(tolower(ch) == 'y') {
+            cout << endl;
+            input();
+        }
+    }
+};
+
+int main() {
+    FracCalc calc;
+    calc.input();
+    return 0;
 }
