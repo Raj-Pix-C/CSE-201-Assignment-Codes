af50d5850487ab353abaf1c4fa649a1c696dc747
Mohmmad Al-Amin Raj <aam731555@gmail.com>
2025-02-19 11:58:01 +0600

    addition of ch7

---
+++ ch7_q01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution{
+private:
+    string s;
+public:
+    void in(){cin >> s;}
+    void reverseit(){
+        reverse(s.begin(), s.end());
+    }
+    void print()const{cout << s << endl;}
+};
+int main()
+{
+    Solution sol;
+    sol.in();
+    sol.reverseit();
+    sol.print();
+    return 0;
+}
